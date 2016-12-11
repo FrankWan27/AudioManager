912004268974a0fc299b58d0957f669d4dae651a
Frank Wan <frankwan27@gmail.com>
2016-12-11 14:58:11 -0800

    Added arg parse stuff

--- AudioManager.py
+++ AudioManager.py
@@ -1,3 +1,22 @@
 import eyed3
-import sys
 import argparse
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', help='path to audio file', default='')
+parser.add_argument('--format', help='naming format of mp3 file', default='%a - %t')
+
+args = parser.parse_args()
+
+fileName = args.file
+fileFormat = args.format
+
+if len(sys.argv) == 1:
+    print('ERROR: Please enter a file name.')
+    exit(-1)
+
+audio = eyed3.load(fileName)
+
+
+
+
