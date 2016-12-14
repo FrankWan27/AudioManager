7857d36690336ee741d0e9047ff6240690628167
Frank Wan <frankwan27@gmail.com>
2016-12-13 22:32:59 -0800

    Added tab system to GUI

--- AudioManager.py
+++ AudioManager.py
@@ -1,6 +1,7 @@
 import Tkinter, tkFileDialog
+import ttk
 from Tool import renameFile
-
+import Tab
 class audioGUI(Tkinter.Tk):
     def __init__(self, parent):
         Tkinter.Tk.__init__(self, parent)
@@ -10,32 +11,44 @@ class audioGUI(Tkinter.Tk):
     def initialize(self):
         self.grid()
         
+        nb = ttk.Notebook(self)
+
+        page1 = ttk.Frame(nb)
+        page2 = ttk.Frame(nb)
+        page3 = ttk.Frame(nb)
+        page4 = ttk.Frame(nb)
+        nb.add(page1, text='File Rename')
+        nb.add(page2, text='Folder Rename')
+        nb.add(page3, text='File Tag Edit')
+        nb.add(page4, text='Folder Tag Edit')
+        nb.grid(column=0, row=0, stick='EW')
+        
         self.file = Tkinter.StringVar()
         
-        self.fileEntry = Tkinter.Entry(self, textvariable=self.file)
-        self.fileEntry.grid(column=1, row=0, sticky='EW')
-        
-        fileLabel = Tkinter.Label(self, text='File Path:')
+        fileEntry = Tkinter.Entry(page1, textvariable=self.file)
+        fileEntry.grid(column=1, row=0, sticky='EW')
+
+        fileLabel = Tkinter.Label(page1, text='File Path:')
         fileLabel.grid(column=0, row=0, sticky='EW')
        
-        self.fileDialog = Tkinter.Button(self, text='Browse...', command=self.openDialog)
-        self.fileDialog.grid(column=2, row=0, sticky='W')
+        fileDialog = Tkinter.Button(page1, text='Browse...', command=self.openDialog)
+        fileDialog.grid(column=2, row=0, sticky='W')
         
         self.format = Tkinter.StringVar()
         self.format.set('%a - %t')
         
-        self.formatEntry = Tkinter.Entry(self, textvariable=self.format)
-        self.formatEntry.grid(column=1, row=1, sticky='EW')
-        
-        formatLabel = Tkinter.Label(self, text='Format: ')
+        formatEntry = Tkinter.Entry(page1, textvariable=self.format)
+        formatEntry.grid(column=1, row=1, sticky='EW')
+                
+        formatLabel = Tkinter.Label(page1, text='Format:')
         formatLabel.grid(column=0, row=1, sticky='EW')
         
-        renameButton = Tkinter.Button(self, text='Rename', command=self.renameClick)
+        renameButton = Tkinter.Button(page1, text='Rename', command=self.renameClick)
         renameButton.grid(column=1, row=2, sticky='W')
 
 
-        self.grid_rowconfigure(1, pad=5)  
-        self.grid_columnconfigure(1, weight=1, minsiz=800)
+        page1.grid_rowconfigure(1, pad=5)  
+        page1.grid_columnconfigure(1, weight=1, minsiz=800)
 
     def renameClick(self):
         renameFile(self.file.get(), self.format.get())
